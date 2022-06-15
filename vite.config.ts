577df6236db9d027ac775975a200abc946e5ae4c
hly1445 <1043817056@qq.com>
2022-06-15 17:45:18 +0800

    feat: add unit test

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import vue from "@vitejs/plugin-vue";
 import path from "path";
 import dts from "vite-plugin-dts";
 import VueSetupExtend from "vite-plugin-vue-setup-extend";
+const { babel } = require('@rollup/plugin-babel')
 
 export default defineConfig({
   // 需要安装babel
@@ -16,5 +17,12 @@ export default defineConfig({
       fileName: "TokenInsightWidget",
       formats: [ "umd" ],
     },
+    rollupOptions: {
+      plugins: [
+        babel({
+          babelHelpers: 'bundled'
+        })
+      ],
+    }
   },
 });
